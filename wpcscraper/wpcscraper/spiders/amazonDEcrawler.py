65a654595a0958cd0f34baf16fc9688baf50a109
Karsten Peucker <Karsten.Peucker@icloud.com>
2019-03-25 22:20:22 +0900

    New try for preparing the use of two serial crawler

--- wpcscraper/wpcscraper/spiders/amazonDEcrawler.py
+++ wpcscraper/wpcscraper/spiders/amazonDEcrawler.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import datetime
-
-from scrapy.http import Request
-
+from scrapy.http import *
+from scrapy.crawler import CrawlerProcess
+from pydispatch import dispatcher
 from ..items import amazonDEcrawler
+
+import json
+import os
 
 class AmazondespiderSpider(scrapy.Spider):
 
@@ -13,7 +15,7 @@ class AmazondespiderSpider(scrapy.Spider):
     name = 'amazonDEcrawler'
     # allowed_domains = ['amazon.de']
     page_number = 2
-
+    urlList = []
     start_urls = [
         'https://www.amazon.de/s/ref=nb_sb_noss?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=qi&rh=i%3Aaps%2Ck%3Aqi'
     # Page 2 https://www.amazon.de/s/ref=sr_pg_2?fst=as%3Aon&rh=k%3Aqi%2Cn%3A562066%2Cn%3A1384526031%2Cn%3A364918031%2Cn%3A364929031%2Cn%3A1385091031&page=2&keywords=qi&ie=UTF8&qid=1553174833
@@ -23,7 +25,7 @@ class AmazondespiderSpider(scrapy.Spider):
         
         # Declarations 
         base_url = 'https://www.amazon.de'
-        urlList = []
+
         
         # Extract the product URL from the initial search page
         prod_urls = response.css('.a-link-normal::attr(href)').getall()
@@ -31,19 +33,35 @@ class AmazondespiderSpider(scrapy.Spider):
         # Parse through the found URLs and parse out the product relevant ones
         for url in prod_urls:
             if url.startswith('/gp/slredirect/'):
-                urlList.append(base_url + url) # Add the base URL of the website and append the results to
-                                               # to a list.
+                if not url.endswith('#customerReviews'):
+                    AmazondespiderSpider.urlList.append(base_url + url) # Add the base URL of the website and append the results to
+                                                   # to a list.
 
-        AmazondespiderSpider.start_urls = list(dict.fromkeys(urlList)) # Remove the duplicates in the list
+            AmazondespiderSpider.urlList = list(dict.fromkeys(AmazondespiderSpider.urlList)) # Remove the duplicates in the list
 
-        print(AmazondespiderSpider.start_urls)
 
-            
-        #Go to second...n page
-        #next_page = 'Page 2 https://www.amazon.de/s/ref=sr_pg_2?fst=as%3Aon&rh=k%3Aqi%2Cn%3A562066%2Cn' \
-        #            '%3A1384526031%2Cn%3A364918031%2Cn%3A364929031%2Cn%3A1385091031&page=' + str(
-        #    AmazondespiderSpider.page_number) + '&keywords=qi&ie=UTF8&qid=1553174833 '
-        #AmazondespiderSpider.page_number += 1 # Increment the page
-        #yield response.follow(next_page, callback=self.parse) # Call the parse function until the last page is reached
 
-        print('**********************************************************************************************HALLO**********************************************************')
+            #Go to second...n page
+            next_page = 'Page 2 https://www.amazon.de/s/ref=sr_pg_2?fst=as%3Aon&rh=k%3Aqi%2Cn%3A562066%2Cn' \
+                        '%3A1384526031%2Cn%3A364918031%2Cn%3A364929031%2Cn%3A1385091031&page=' + str(
+                AmazondespiderSpider.page_number) + '&keywords=qi&ie=UTF8&qid=1553174833 '
+            AmazondespiderSpider.page_number += 1 # Increment the page
+            yield response.follow(next_page, callback=self.parse) # Call the parse function until the last page is reached
+
+        with open('data.txt', 'w') as outfile:
+            json.dump(AmazondespiderSpider.urlList, outfile)
+
+    print('**********************************************************************************************HALLO**********************************************************')
+
+    with open('data.txt') as json_file:
+        data = json.load(json_file)
+        for p in data:
+            print(p)
+
+    print('****Printing Done****')
+
+    if os.path.exists('data.txt'):
+        os.remove('data.txt')
+        print('***File deleted***')
+    else:
+        print('File does not exist !')
